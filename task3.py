c9129effab3a870b7de903ab7bda933fc9588388
Nataliia <nataliyatrishechkina@mail.ru>
2023-09-17 21:39:33 +0300

    add code for task3

--- task3.py
+++ task3.py
@@ -4,19 +4,19 @@
 # from random import randint
 # num = randint(LOWER_LIMIT, UPPER_LIMIT)
 
-# from random import randint
-# num = randint(0, 1000)
-# count = 10
-# entered_number = float(input("Введите число: "))
-# while entered_number != num and count > 1:
-#     count -= 1
-#     if entered_number > num:
-#         print("Введите число меньше")
-#         print("Осталось вот столько попыток:", count)
-#     elif entered_number < num:
-#         print("Введите число больше")
-#         print("Осталось вот столько попыток:", count)
-#     entered_number = float(input("Повтори попытку: "))
-# print ("Это было число", num)
-# if entered_number == num:
-#     print("Вы угадали!")
+from random import randint
+num = randint(0, 1000)
+count = 10
+entered_number = float(input("Введите число: "))
+while entered_number != num and count > 1:
+    count -= 1
+    if entered_number > num:
+        print("Введите число меньше")
+        print("Осталось вот столько попыток:", count)
+    elif entered_number < num:
+        print("Введите число больше")
+        print("Осталось вот столько попыток:", count)
+    entered_number = float(input("Повтори попытку: "))
+print ("Это было число", num)
+if entered_number == num:
+    print("Вы угадали!")
